bbfdb2fceab6662364f24df6511d614066e0a3de
gehad <gehadadel6111@gmail.com>
2025-08-08 23:28:41 +0300

    initial

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import MyFirstComponent from './MyFirstcomponent'
+
+
+function App() {
+
+  return (
+    <>
+    <MyFirstComponent />
+    </>
+  )
+}
+
+
+export default App
---
+++ my-react-app/src/MyFirstcomponent.jsx
@@ -0,0 +1,11 @@
+export default function MyFirstComponent(){
+  return(
+    <>
+        <h1>Hello react world</h1>
+        <h2>Hiiii</h2>
+        <button>Click me</button>
+    </>
+  )
+}
+
+
