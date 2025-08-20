dfaae8ce53eed926bc93c5e95ad5627f37b6b7d7
gehad <gehadadel6111@gmail.com>
2025-08-20 18:51:37 +0300

    moving the dialoug modal up

--- ToDoListProject/src/Components/ToDo.jsx
+++ ToDoListProject/src/Components/ToDo.jsx
@@ -1,24 +1,18 @@
 import CheckIcon from '@mui/icons-material/Check';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
-import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
 import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
-import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { TodosContext } from '../Context/TodosContext';
 
 
-const ToDo = ({ todo }) => {
+const ToDo = ({ todo, handleEditClick, handleDeleteClick }) => {
     const { todos, setTodos } = useContext(TodosContext)
+
     function handleCheckClick() {
         const checkedTodos = todos.map((t) => {
             if (t.id == todo.id) {
@@ -30,104 +24,15 @@ const ToDo = ({ todo }) => {
         localStorage.setItem("todos", JSON.stringify(checkedTodos))
     }
 
-    const [openDeleteDialog, setOpenDeleteDialog] = useState(false)
-    const [openEditDialog, setOpenEditDialog] = useState(false)
-    const [updatedTask, setUpdatedTask] = useState({ title: todo.title, details: todo.details })
+    function handleEditDialog(todo) {
+        handleEditClick(todo)
+    }
+    function handleDeleteDialog(todo) {
+        handleDeleteClick(todo)
+    }
 
-    function handleCloseDelete() {
-        setOpenDeleteDialog(false)
-    }
-    function handleDeleteClick() {
-        setOpenDeleteDialog(true)
-    }
-    function handleEditClick() {
-        setOpenEditDialog(true)
-    }
-    function handleCloseEdit() {
-        setOpenEditDialog(false)
-    }
-    function handleDeleteSubmit() {
-        const updatedTodos = todos.filter((t) => {
-            return t.id != todo.id
-        })
-        setTodos(updatedTodos)
-        localStorage.setItem("todos", JSON.stringify(updatedTodos))
-    }
-    function handleUpdateSubmit() {
-        const updatedTasks = todos.map((t) => {
-            if (todo.id == t.id) {
-                return { ...todo, title: updatedTask.title, details: updatedTask.details }
-            } else {
-                return t
-            }
-        })
-        setTodos(updatedTasks)
-        setOpenEditDialog(false)
-        localStorage.setItem("todos", JSON.stringify(updatedTasks))
-    }
     return (
         <>
-            {/* delete Dialog */}
-            <Dialog
-                open={openDeleteDialog}
-                onClose={handleCloseDelete}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-                style={{ direction: "rtl" }}
-            >
-                <DialogTitle id="alert-dialog-title">
-                    {"هل أنت متأكد أنك تريد حذف المهمه؟"}
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        لا يمكنك التراجع عن الحذف بعد إتمامه
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleCloseDelete}>إلغاء</Button>
-                    <Button onClick={handleDeleteSubmit} autoFocus style={{ color: "#b23c17" }}>
-                        نعم ,أريد الحذف
-                    </Button>
-                </DialogActions>
-            </Dialog>
-            {/* edit Dialog */}
-            <Dialog open={openEditDialog} onClose={handleCloseEdit} style={{ direction: "rtl", width: "100%" }}>
-                <DialogTitle>{"تعديل المهمه"}</DialogTitle>
-                <DialogContent>
-                    <TextField
-                        autoFocus
-                        required
-                        margin="dense"
-                        id="name"
-                        name="عنوان المهمه"
-                        label="عنوان المهمه"
-                        type="text"
-                        fullWidth
-                        variant="standard"
-                        value={updatedTask.title}
-                        onChange={(e) => setUpdatedTask({ ...updatedTask, title: e.target.value })}
-                    />
-                    <TextField
-                        autoFocus
-                        required
-                        margin="dense"
-                        id="name"
-                        name="التفاصيل"
-                        label="التفاصيل"
-                        type="text"
-                        fullWidth
-                        variant="standard"
-                        value={updatedTask.details}
-                        onChange={(e) => setUpdatedTask({ ...updatedTask, details: e.target.value })}
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleCloseEdit}>إلغاء</Button>
-                    <Button onClick={handleUpdateSubmit} type="submit" form="subscription-form" style={{ color: "#b23c17" }}>
-                        تعديل
-                    </Button>
-                </DialogActions>
-            </Dialog>
             <Card className='todoCard' sx={{ minWidth: 275, backgroundColor: "#C1D8C3", marginTop: 2 }}>
                 <CardContent>
 
@@ -145,10 +50,10 @@ const ToDo = ({ todo }) => {
                             <IconButton onClick={handleCheckClick} className='btnhover check' aria-label="check" style={{ padding: "3px", border: "1px solid #4caf50", backgroundColor: todo.isCompleted ? "#4caf50" : "white", color: todo.isCompleted ? "white" : "#4caf50" }}>
                                 <CheckIcon />
                             </IconButton>
-                            <IconButton onClick={handleEditClick} className='btnhover edit' aria-label="edit" style={{ padding: "3px", border: "1px solid #1769aa", backgroundColor: "white", color: "#1769aa" }}>
+                            <IconButton onClick={() => { handleEditDialog(todo) }} className='btnhover edit' aria-label="edit" style={{ padding: "3px", border: "1px solid #1769aa", backgroundColor: "white", color: "#1769aa" }}>
                                 <EditIcon />
                             </IconButton>
-                            <IconButton onClick={handleDeleteClick} className='btnhover delete' aria-label="delete" style={{ padding: "3px", border: "1px solid #b23c17", backgroundColor: "white", color: "#b23c17" }}>
+                            <IconButton onClick={() => { handleDeleteDialog(todo) }} className='btnhover delete' aria-label="delete" style={{ padding: "3px", border: "1px solid #b23c17", backgroundColor: "white", color: "#b23c17" }}>
                                 <DeleteIcon />
                             </IconButton>
                         </Grid>
--- ToDoListProject/src/Components/ToDoList.jsx
+++ ToDoListProject/src/Components/ToDoList.jsx
@@ -2,6 +2,11 @@ import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Container from '@mui/material/Container';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
@@ -16,6 +21,11 @@ import ToDo from './ToDo';
 
 
 const ToDoList = () => {
+    const [openDeleteDialog, setOpenDeleteDialog] = useState(false)
+    const [openEditDialog, setOpenEditDialog] = useState(false)
+    const [deleteId, setDeleteId] = useState(null)
+    const [editeId, setEditId] = useState(null)
+    const [updatedTask, setUpdatedTask] = useState({ title: "", details: "" })
     const { todos, setTodos } = useContext(TodosContext)
     const [title, setTitle] = useState("")
     const [details, setDetails] = useState("")
@@ -47,7 +57,7 @@ const ToDoList = () => {
         todosRendered = todos
     }
     const todoJsx = todosRendered.map((t) => {
-        return <ToDo key={t.id} todo={t} />
+        return <ToDo key={t.id} todo={t} handleDeleteClick={(todo) => { setDeleteId(todo.id); setOpenDeleteDialog(true) }} handleEditClick={(todo) => { setEditId(todo.id); setOpenEditDialog(true); setUpdatedTask({ title: todo.title, details: todo.details }) }} />
     })
     function handleAddClick() {
         const newTodo = {
@@ -68,9 +78,100 @@ const ToDoList = () => {
         const storageTodos = JSON.parse(localStorage.getItem("todos")) || []
         setTodos(storageTodos)
     }, [])
+
+    // dialog handelers
+    function handleCloseDelete() {
+        setOpenDeleteDialog(false)
+    }
+    function handleCloseEdit() {
+        setOpenEditDialog(false)
+    }
+
+    function handleDeleteSubmit() {
+        const updatedTodos = todos.filter((t) => {
+            return t.id != deleteId
+        })
+        setTodos(updatedTodos)
+        localStorage.setItem("todos", JSON.stringify(updatedTodos))
+        setOpenDeleteDialog(false)
+    }
+    function handleUpdateSubmit() {
+        const updatedTasks = todos.map((t) => {
+            if (editeId == t.id) {
+                return { ...t, title: updatedTask.title, details: updatedTask.details }
+            } else {
+                return t
+            }
+        })
+        setTodos(updatedTasks)
+        setOpenEditDialog(false)
+        localStorage.setItem("todos", JSON.stringify(updatedTasks))
+    }
     return (
 
         <>
+
+            {/* delete Dialog */}
+            <Dialog
+                open={openDeleteDialog}
+                onClose={handleCloseDelete}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+                style={{ direction: "rtl" }}
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"هل أنت متأكد أنك تريد حذف المهمه؟"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        لا يمكنك التراجع عن الحذف بعد إتمامه
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseDelete}>إلغاء</Button>
+                    <Button onClick={handleDeleteSubmit} autoFocus style={{ color: "#b23c17" }}>
+                        نعم ,أريد الحذف
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            {/* edit Dialog */}
+            <Dialog open={openEditDialog} onClose={handleCloseEdit} style={{ direction: "rtl", width: "100%" }}>
+                <DialogTitle>{"تعديل المهمه"}</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        required
+                        margin="dense"
+                        id="name"
+                        name="عنوان المهمه"
+                        label="عنوان المهمه"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        value={updatedTask.title}
+                        onChange={(e) => setUpdatedTask({ ...updatedTask, title: e.target.value })}
+                    />
+                    <TextField
+                        autoFocus
+                        required
+                        margin="dense"
+                        id="name"
+                        name="التفاصيل"
+                        label="التفاصيل"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        value={updatedTask.details}
+                        onChange={(e) => setUpdatedTask({ ...updatedTask, details: e.target.value })}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseEdit}>إلغاء</Button>
+                    <Button onClick={handleUpdateSubmit} type="submit" form="subscription-form" style={{ color: "#b23c17" }}>
+                        تعديل
+                    </Button>
+                </DialogActions>
+            </Dialog>
             <Container maxWidth="sm" style={{ textAlign: "center", direction: "rtl", marginTop: "50px" }}>
                 <Card sx={{ minWidth: 275, overflow: "scroll", maxHeight: '520px', scrollbarColor: "white white" }}>
                     <CardContent>
